91dab461a1e487fd3737851e47b0635e9ad29d34
BrianMartelA <164060830+BrianMartelA@users.noreply.github.com>
2024-04-17 22:31:29 -0400

    Add files via upload

--- actividad/script/script.js
+++ actividad/script/script.js
@@ -1,3 +1,18 @@
+
+var nombre = document.getElementById('nombre')
+var error = document.getElementById('error')
+error.style.color = 'red';
+
+
+function enviarForm(){
+    console.log('Enviando formulario...')
+    
+    var mensajeError = []
+
+    if(nombre.value === null || nombre.value === ''){
+        mensajeError.push('Ingrese un nombre valido')
+    }
+}
 var form = document.getElementById('formulario');
     form.addEventlistener('submit', function(evt){
         evt.preventDefault();
@@ -13,27 +28,9 @@ var form = document.getElementById('formulario');
         if(apellido_mat.value === null || apellido_mat.length < 3 || apellido_mat.length > 20){
             mensajesError.push("Ingresa tu apellido materno correctamente")
         }
-
-        if(edad.value<18 || edad.value>35 || edad.length === null){
-            mensajesError.push("Ingrese edad mayor a 18 años y menor a 35");
-            console.log('menor de edad');
-        
-
-    }
-    else{
-        mensajesError.push("Edad ingresada correctamente");
-        console.log('edad correcta');
-    }
-    if(tel.length< 9 || tel.length>12 ){
-        mensajesError.push("Su numero debe tener entre 9 a 12 digitos y seguir el formato correcto");
-        console.log('numero incorrecto');
-    }
-    else{
-        mensajesError.push("numero almacenado correctamente");
-        console.log('numero correcto');
-    }
         //---------------------
 
         error.innerHTML = mensajesError.join(' , ');
 
     });
+
